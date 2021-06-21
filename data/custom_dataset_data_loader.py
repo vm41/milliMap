e127db3c6fcc0a9e24bf8da1f313dafeb7524a40
vm41 <73190179+vm41@users.noreply.github.com>
2021-06-21 03:20:32 -0400

    Update custom_dataset_data_loader.py

--- data/custom_dataset_data_loader.py
+++ data/custom_dataset_data_loader.py
@@ -2,30 +2,32 @@ import torch.utils.data
 from data.base_data_loader import BaseDataLoader
 
 
+ # Create data set
 def CreateDataset(opt):
     dataset = None
     from data.aligned_dataset import AlignedDataset
     dataset = AlignedDataset()
 
-    print("dataset [%s] was created" % (dataset.name()))
-    dataset.initialize(opt)
-    return dataset
+    print("dataset [%s] was created" % (dataset.name())) # The name of the print dataset is ‘AlignedDataset’
+    dataset.initialize(opt) # Initialize data set parameter
+    return dataset # Return the created dataset
 
-class CustomDatasetDataLoader(BaseDataLoader):
+ # Load data set
+class CustomDatasetDataLoader(BaseDataLoader): 
     def name(self):
         return 'CustomDatasetDataLoader'
 
     def initialize(self, opt):
-        BaseDataLoader.initialize(self, opt)
-        self.dataset = CreateDataset(opt)
-        self.dataloader = torch.utils.data.DataLoader(
+        BaseDataLoader.initialize(self, opt) # initialization parameters
+        self.dataset = CreateDataset(opt) # create data set
+        self.dataloader = torch.utils.data.DataLoader( # Load the created data set and customize related parameters
             self.dataset,
             batch_size=opt.batchSize,
             shuffle=not opt.serial_batches,
             num_workers=int(opt.nThreads))
 
     def load_data(self):
-        return self.dataloader
+        return self.dataloader # return data set
 
     def __len__(self):
-        return min(len(self.dataset), self.opt.max_dataset_size)
+        return min(len(self.dataset), self.opt.max_dataset_size) # return the length of the loaded data set and the maximum load capacity allowed by an epoch
