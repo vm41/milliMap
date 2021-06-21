ef080d97869f42b5ade83fda729481690ba66bf9
vm41 <73190179+vm41@users.noreply.github.com>
2021-06-21 03:15:25 -0400

    Update data_loader.py

--- data/data_loader.py
+++ data/data_loader.py
@@ -1,7 +1,8 @@
 
+# Create data set loading main function
 def CreateDataLoader(opt):
     from data.custom_dataset_data_loader import CustomDatasetDataLoader
     data_loader = CustomDatasetDataLoader()
-    print(data_loader.name())
-    data_loader.initialize(opt)
+    print(data_loader.name()) # The name returned is "CustomDatasetDataLoader"
+    data_loader.initialize(opt) # initialization parameters
     return data_loader
