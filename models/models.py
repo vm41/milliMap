1a44893d4b078d4a7cee5f2d7f1dd3ee0c67f7f3
vm41 <73190179+vm41@users.noreply.github.com>
2021-06-21 06:29:26 -0400

    Update models.py

--- models/models.py
+++ models/models.py
@@ -1,22 +1,26 @@
 ### Copyright (C) 2017 NVIDIA Corporation. All rights reserved. 
 ### Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode).
 import torch
+# Create a model and return to the model
 
 def create_model(opt):
-    if opt.model == 'pix2pixHD':
+    if opt.model == 'pix2pixHD': # select pix2pixHD model
         from .pix2pixHD_model import Pix2PixHDModel, InferenceModel
-        if opt.isTrain:
+        if opt.isTrain: # True for training
             model = Pix2PixHDModel()
-        else:
+        else: # Otherwise, if only forward propagation is used for demonstration, it is False
+
             model = InferenceModel()
-    else:
+    else: # select UIModel model
     	from .ui_model import UIModel
     	model = UIModel()
-    model.initialize(opt)
-    if opt.verbose:
-        print("model [%s] was created" % (model.name()))
+    model.initialize(opt) # Model initialization parameters
+    if opt.verbose: # The default is false, indicating that no model has been saved before
+        print("model [%s] was created" % (model.name())) # print label2city model was created
+
 
     if opt.isTrain and len(opt.gpu_ids):
-        model = torch.nn.DataParallel(model, device_ids=opt.gpu_ids)
+        model = torch.nn.DataParallel(model, device_ids=opt.gpu_ids) # Multi-GPU training
+
 
     return model
